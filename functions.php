5e869fbf18f947e655445467d0ae5f454bd86e32
richard <rgoodie@users.noreply.github.com>
2016-07-18 15:14:49 -0500

    first

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carbon
+ * Date: 7/18/16
+ * Time: 2:40 PM
+ */
+
+
+
+add_action('wp_enqueue_scripts', 'nugm_soc_theme_enqueue_styles');
+function nugm_soc_theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child', get_stylesheet_directory_uri() . '/style.css');
+}
