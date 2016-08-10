d2c4881302791f0d66f491d950e64037ba447a69
richard <rgoodie@users.noreply.github.com>
2016-08-10 15:22:52 -0500

    make all pages 'standard-page' except home per Alex's recommendation

--- functions.php
+++ functions.php
@@ -16,3 +16,17 @@ function nugm_soc_theme_enqueue_styles()
 
 
 }
+
+
+
+/* Add standard-page class to everything except homepage. Thanks to Alex Miner */
+add_filter( 'body_class', 'nu_gm_nuamps_child_body_classes', 11 );
+function nu_gm_nuamps_child_body_classes( $classes ) {
+
+
+   if ( !is_front_page() && $key = array_search('landing-page', $classes) ) {
+        $classes[] = 'standard-page';
+    }
+
+    return $classes;
+}
