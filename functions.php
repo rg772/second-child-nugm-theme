2780ca4f3cadb8635ecc128969b6ba8f3c141607
rgoodie <rgoodie@users.noreply.github.com>
2017-05-17 14:53:41 -0500

    SOC level options

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@
  * Time: 2:40 PM
  */
 
-
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('nugm_soc_style', get_stylesheet_directory_uri() . '/style.css');
@@ -100,6 +99,52 @@ add_action('widgets_init', function() {
     ]);
 
 });
+
+// adds class to main navigation menu
+add_filter( 'nu_gm_top_nav_classes', function($classes) {
+    if (get_theme_mod('soc_use_narrow_branding', false)) {
+        $classes[] = 'narrow-dropdown';
+        return $classes;
+    }
+    else {
+        return '';
+    }
+});
+
+
+
+/*
+* Add SoC options 
+*/
+add_action('customize_register', function($wp_customize) {
+   
+   // SoC Section
+  $wp_customize->add_section('SOC', array(
+    'title' => 'SOC level options',
+    'priority' => 1700,
+  ));
+
+   // Use narrow branding in the main nav menu
+  $wp_customize->add_setting( 'soc_use_narrow_branding' , array(
+    'default' => false,
+    'sanitize_callback' => 'nu_gm_sanitize_checkbox',
+  ));
+  $wp_customize->add_control(
+    new WP_Customize_Control(
+      $wp_customize,
+      'show_socs',
+      array(
+        'label' => 'Use narrow-branding menus',
+        'description'=>'This insructs the theme to insert <code>narrow-branding</code> into the top nav as a class.',
+        'section' => 'SOC',
+        'type' => 'checkbox',
+        'settings' => 'soc_use_narrow_branding',
+      )
+          )
+  );
+});
+
+
 
 
 /*
